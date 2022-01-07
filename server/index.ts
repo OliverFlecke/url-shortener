cb76cdfbc93ab5afe6fedb417538a62f49bc54a8
Oliver Fleckenstein <oliverfl@live.dk>
2022-01-07 11:32:27 +0100

    feat: setup with express server

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,23 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import { createServer } from 'http';
+import express, { Response } from 'express';
+import next from 'next';
 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+nextApp.prepare().then(() => {
+	const app = express();
+	const server = createServer(app);
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+	app.get('/hello', async (_, res: Response) => {
+		res.send('Hello world!');
+	});
+
+	app.all('*', (req, res) => nextHandler(req, res));
+
+	server.listen(port, () => {
+		console.log(`>> Ready on port ${port}`);
+	});
+});
