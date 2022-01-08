bbf1d30fa8e5a03f62f1382d341512ba8be4803a
Oliver Fleckenstein <oliverfl@live.dk>
2022-01-08 02:13:06 +0100

    style: Initial styling for URL list

--- src/components/ShortenedUrlList.tsx
+++ src/components/ShortenedUrlList.tsx
@@ -11,13 +11,20 @@ interface ShortenedUrlListProps {
 
 const ShortenedUrlList: React.FC<ShortenedUrlListProps> = ({ urls }) => {
 	return (
-		<ul>
-			{urls.map((x) => (
-				<li key={x.name}>
-					{x.name} - <a href={x.url.toString()}>{x.url.toString()}</a>
-				</li>
-			))}
-		</ul>
+		<div className="max-w-screen m-4">
+			<h3 className="text-xl">Active links</h3>
+			<ul className="rounded-md mt-2 p-4 space-y-4 bg-gray-700">
+				{urls.map((x) => (
+					<li
+						key={x.name}
+						className="rounded-md bg-green-800 p-4 flex justify-between"
+					>
+						<span>{x.name}</span>
+						<a href={x.url.toString()}>{x.url.toString()}</a>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 };
 
