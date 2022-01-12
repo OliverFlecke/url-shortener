85fe512b5b16138d9ddcbf0907b4a24571531568
Oliver Fleckenstein <oliverfl@live.dk>
2022-01-12 20:50:16 +0100

    feat: Updated UI with button to delete link

--- src/components/ShortenedUrlList.tsx
+++ src/components/ShortenedUrlList.tsx
@@ -1,4 +1,4 @@
-import { arrowDown, copy } from 'ionicons/icons';
+import { arrowDown, copy, trash } from 'ionicons/icons';
 import React, {
 	forwardRef,
 	useCallback,
@@ -11,35 +11,71 @@ import Icon from './common/Icon';
 import { Selector } from './common/Selector';
 import Tooltip from './common/Tooltip';
 
+type LinkType = 'All' | 'Private';
+
 const NoLinks: React.FC = () => (
-	<span className="text-secondary">No active links</span>
+	<div className="text-secondary">No active links</div>
 );
 
-const List: React.FC<{ urls: ShortenedUrl[] }> = ({ urls }) => (
+const List: React.FC<{
+	urls: ShortenedUrl[];
+	urlType: LinkType;
+	refresh: () => void;
+}> = ({ urls, urlType, refresh }) => (
 	<ul className="rounded-md mt-2 space-y-4">
 		{urls.map((x) => (
-			<UrlRow key={x.name} url={x} />
+			<UrlRow key={x.name} url={x} urlType={urlType} refresh={refresh} />
 		))}
 	</ul>
 );
 
-const UrlRow: React.FC<{ url: ShortenedUrl }> = ({ url }) => {
+const UrlRow: React.FC<{
+	url: ShortenedUrl;
+	urlType: LinkType;
+	refresh: () => void;
+}> = ({ url, urlType, refresh }) => {
 	const shortUrl = `${window.location.origin}/s/${url.name}`;
 	const copyUrl = useCallback(
 		() => navigator.clipboard.writeText(shortUrl),
 		[shortUrl]
 	);
+	const deleteUrl = useCallback(async () => {
+		try {
+			const res = await fetch(`/s/${url.name}`, {
+				method: 'DELETE',
+			});
+			if (res.status === 200) {
+				refresh();
+			} else {
+				alert(`Failed to delete ${url.url.toString()}`);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}, [refresh, url.name, url.url]);
 
 	return (
 		<li className="rounded-md bg-green-800 p-4 flex flex-col justify-between items-center">
 			<span className="flex w-full justify-between space-x-2 sm:w-auto">
 				<span>{shortUrl}</span>
-				<button onClick={copyUrl} className="flex items-center">
-					<Tooltip>
-						<Icon icon={copy} />
-						<Tooltip.Text>Copy to clipboard</Tooltip.Text>
-					</Tooltip>
-				</button>
+				<div className="flex flex-row space-x-4">
+					<button
+						onClick={copyUrl}
+						aria-label="Copy url"
+						className="flex items-center"
+					>
+						<Tooltip>
+							<Icon icon={copy} />
+							<Tooltip.Text>Copy to clipboard</Tooltip.Text>
+						</Tooltip>
+					</button>
+					<button onClick={deleteUrl} aria-label="Delete url">
+						<Icon
+							icon={trash}
+							className={urlType === 'Private' ? '' : 'hidden'}
+						/>
+					</button>
+				</div>
 			</span>
 			<Icon icon={arrowDown} />
 			<a href={url.url.toString()} className="underline text-sky-300">
@@ -59,9 +95,10 @@ const UrlRow: React.FC<{ url: ShortenedUrl }> = ({ url }) => {
  */
 const ShortenedUrlListContainer = forwardRef((_, ref) => {
 	const [urls, setUrls] = useState<ShortenedUrl[]>([]);
-	const [urlType, setUrlType] = useState<'All' | 'Private'>('All');
+	const [urlType, setUrlType] = useState<LinkType>('All');
 
 	const getUrls = useCallback(async () => {
+		setUrls([]);
 		const res = await fetch(urlType === 'Private' ? '/s?private' : '/s/');
 		if (res.status === 200) {
 			setUrls(await res.json());
@@ -89,7 +126,11 @@ const ShortenedUrlListContainer = forwardRef((_, ref) => {
 					selected={urlType}
 					setState={setUrlType}
 				/>
-				{urls.length === 0 ? <NoLinks /> : <List urls={urls} />}
+				{urls.length === 0 ? (
+					<NoLinks />
+				) : (
+					<List urls={urls} urlType={urlType} refresh={getUrls} />
+				)}
 			</div>
 		</div>
 	);
